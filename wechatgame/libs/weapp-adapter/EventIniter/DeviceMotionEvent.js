d2ab11448cd43c11469d4cbf3debb87055eec812
PP <chinese1236@qq.com>
2018-06-04 15:07:44 +0800

    adapt device motion event
    fix babel
    
    
    update Factor

--- wechatgame/libs/weapp-adapter/EventIniter/DeviceMotionEvent.js
+++ wechatgame/libs/weapp-adapter/EventIniter/DeviceMotionEvent.js
@@ -6,7 +6,25 @@ export default class DeviceMotionEvent {
 }
 
 wx.onAccelerometerChange && wx.onAccelerometerChange(function (res) {
-  var deviceMotionEvent = new DeviceMotionEvent();
+  let deviceMotionEvent = new DeviceMotionEvent();
+  let gravityFactor = 10;
+  let visibleRect = cc.view._visibleRect;
+  if (visibleRect.height / visibleRect.width < 1) {
+    // landscape view
+    let tmp = res.x;
+    res.x = res.y;
+    res.y = tmp;
+    
+    res.x *= gravityFactor;
+    res.y *= -gravityFactor;
+
+    // TODO adjust x y axis when the view flips upside down
+  }
+  else {
+    // portrait view
+    res.x *= -gravityFactor;
+    res.y *= -gravityFactor;
+  }
   deviceMotionEvent.accelerationIncludingGravity = res;
 
   document.dispatchEvent(deviceMotionEvent);
