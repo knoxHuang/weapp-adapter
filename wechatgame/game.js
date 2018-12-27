a78cf95b49006f8de4989c47b34c1d508fc3b3f8
Santy-Wang <wh19930829@qq.com>
2018-12-27 16:29:47 +0800

    add subpackage pipe to transform url (#57)

--- wechatgame/game.js
+++ wechatgame/game.js
@@ -4,6 +4,7 @@ window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
 require('src/settings');
 var settings = window._CCSettings;
+var SubPackPipe = require('./libs/subpackage-pipe');
 require('main');
 require(settings.debug ? 'cocos2d-js.js' : 'cocos2d-js-min.js');
 require('./libs/engine/index.js');
@@ -12,6 +13,11 @@ wxDownloader.REMOTE_SERVER_ROOT = "";
 wxDownloader.SUBCONTEXT_ROOT = "";
 var pipeBeforeDownloader = cc.loader.md5Pipe || cc.loader.assetLoader;
 cc.loader.insertPipeAfter(pipeBeforeDownloader, wxDownloader);
+
+if (settings.subpackages) {
+    var subPackPipe = new SubPackPipe(settings.subpackages);
+    cc.loader.insertPipeAfter(pipeBeforeDownloader, subPackPipe);
+}
 
 if (cc.sys.browserType === cc.sys.BROWSER_TYPE_WECHAT_GAME_SUB) {
     require('./libs/sub-context-adapter');
