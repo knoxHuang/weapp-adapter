3718e4f0d4423d840b5f9bef47bc7dc0102c0461
Knox <knox.h@qq.com>
2019-06-24 19:18:16 +0800

    fix audio replay invalid for ios (#102)
    * fix audio replay invalid for ios
    
    * add adaptor audio stop tips

--- wechatgame/libs/engine/Audio.js
+++ wechatgame/libs/engine/Audio.js
@@ -1,9 +1,12 @@
 (function () {
-    if (!(cc && cc.Audio)) {
+    if (!(cc && cc.Audio && cc.sys.platform === cc.sys.ANDROID)) {
         return;
     }
+
     cc.Audio.prototype.stop = function () {
         if (!this._element) return;
+        // 由于 web 端没有 stop 接口，是通过 pause + currentTime = 0 的方式进行 stop 的
+        // 但是在微信小游戏安卓平台上，pause 后是无法设置 currentTime 为 0 的，从而进行适配
         this._element.stop();
         this._element.currentTime = 0;
         this._unbindEnded();
